60a57dc085e4a2cc8eb658a8802a9be6c86d077e
Yubin Bai <ybai@thrillist.com>
2014-03-26 09:36:41 -0400

    valid number

--- validNumber/main.py
+++ validNumber/main.py
@@ -7,23 +7,22 @@
 # "1 a" => false
 # "2e10" => true
 # Note: It is intended for the problem statement to be ambiguous. You
-# should gather all requirements up front before implementing one.
+# should gather all requirements up front before implementing one
+import re
 
-'''
-Created on May 17, 2013
-@author: Yubin Bai
-'''
+class Solution:
+    # @param s, a string
+    # @return a boolean
+    def isNumber(self, s):
+        s = s.strip()
+        m = re.match(r'[+-]?(([0-9]+\.?[0-9]*)|(\.[0-9]+))([Ee][+-]?[0-9]+)?', s)
+        if m:
+            return m.start() == 0 and m.end() == len(s)
+        else:
+            return False
 
-
-def isValid(numberStr):
-    s = numberStr.strip()
-    import re
-    m = re.match(r'([0-9]*\.?[0-9]*)([Ee][+-]?[0-9]+)?', s)
-    if m:
-        return m.start() == 0 and m.end() == len(s)
-    else:
-        return False
 if __name__ == '__main__':
-    data = ["0", " 0.1 ", "abc", "1 a", "2e10"]
+    data = ["0", " 0.1 ", "abc", "1 a", "2e10", ".", ".1"]
+    s = Solution()
     for i in data:
-        print(isValid(i))
+        print(s.isNumber(i))
