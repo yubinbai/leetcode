21b6ae0ab965caa6951503511788ac38931482a2
Yubin Bai <baiyubin@gmail.com>
2014-03-26 23:12:21 -0400

    palindrom partition

---
+++ palindromePartition/Solution.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class Solution {
+    ArrayList<ArrayList<String>> ret = new ArrayList<ArrayList<String>>();
+    Stack<String> curr = new Stack<String>();
+
+    public ArrayList<ArrayList<String>> partition(String s) {
+        dfs(s, 0);
+        return ret;
+    }
+
+    public void dfs(String s, int pos) {
+        if (pos == s.length()) {
+            ArrayList<String> arr = new ArrayList<String>();
+            for (String str : curr)
+                arr.add(str);
+            ret.add(arr);
+            return;
+        }
+        for (int i = pos + 1; i <= s.length(); i++) {
+            String sub = s.substring(pos, i);
+            if (isPalindrome(sub)) {
+                curr.push(sub);
+                dfs(s, i);
+                curr.pop();
+            }
+        }
+    }
+
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.partition("aab"));
+    }
+}
