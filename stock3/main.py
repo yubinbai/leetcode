4eb9e63b21d5f02e55f30fd0a94065898804f43c
Yubin Bai <baiyubin@gmail.com>
2014-03-31 20:51:05 -0400

    stock

--- stock3/main.py
+++ stock3/main.py
@@ -9,35 +9,45 @@ Created on 2013-5-13
 @author: Yubin Bai
 '''
 
+class Solution:
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        size = len(prices)
+        if size <= 1:
+            return 0
+        if size == 2:
+            if prices[0] < prices[1]:
+                return prices[1] - prices[0]
+            else:
+                return 0
+        # find the best from 0 .. i
+        bestLeft = [0] * size  # bestLeft[0] is empty
+        currProfit = maxProfit = 0
+        currMin = prices[0]
+        for i in range(1, size):
+            currMin = min(currMin, prices[i])
+            currProfit = prices[i] - currMin
+            maxProfit = max(currProfit, maxProfit)
+            bestLeft[i] = maxProfit
 
-def bestTwoTransactions(data):
-    size = len(data)
-    # find the best from 0 .. i
-    bestLeft = [0] * size  # bestLeft[0] is empty
-    currProfit = maxProfit = 0
-    currMin = data[0]
-    for i in range(1, size):
-        currMin = min(currMin, data[i])
-        currProfit = data[i] - currMin
-        maxProfit = max(currProfit, maxProfit)
-        bestLeft[i] = maxProfit
+        # find the best from i .. n-1
+        bestRight = [0] * size  # bestRight[size-1] is empty
+        currProfit = maxProfit = 0
+        currMax = prices[size - 1]
+        for i in range(size - 1, -1, -1):
+            currMax = max(currMax, prices[i])
+            currProfit = currMax - prices[i]
+            maxProfit = max(currProfit, maxProfit)
+            bestRight[i] = maxProfit
 
-    # find the best from i .. n-1
-    bestRight = [0] * size  # bestRight[size-1] is empty
-    currProfit = maxProfit = 0
-    currMax = data[size - 1]
-    for i in range(size - 1, -1, -1):
-        currMax = max(currMax, data[i])
-        currProfit = currMax - data[i]
-        maxProfit = max(currProfit, maxProfit)
-        bestRight[i] = maxProfit
-
-    # find the best pair
-    maxSum = 0
-    for i in range(1, size - 1):
-        maxSum = max(maxSum, bestLeft[i] + bestRight[i + 1])
-    return maxSum
+        # find the best pair
+        maxSum = 0
+        for i in range(1, size - 1):
+            maxSum = max(maxSum, bestLeft[i] + bestRight[i + 1])
+        return maxSum
 
 if __name__ == '__main__':
-    data = [1, 9, 10, 4, 15, 100]
-    print(bestTwoTransactions(data))
+    prices = [1, 9, 10, 4, 15, 100]
+    s = Solution()
+    print(s.maxProfit(prices))
