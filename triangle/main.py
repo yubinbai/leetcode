05cb237aaf37d2130b300fa11b44f45e3df01c67
Yubin Bai <baiyubin@gmail.com>
2014-04-24 00:28:56 -0400

    triangle

--- triangle/main.py
+++ triangle/main.py
@@ -2,14 +2,16 @@ class Solution:
     # @param triangle, a list of lists of integers
     # @return an integer
     def minimumTotal(self, triangle):
-        prev = list(triangle[-1])
-        for rowI in range(len(triangle)-1, -1, -1):
-            row = triangle[rowI]
-            curr = []
-            for i in range(prev):
-                
+        if len(triangle) == 0:
+            return 0
+        for rowI in range(len(triangle)-1, 0, -1):
+            r = triangle[rowI]
+            rr = triangle[rowI - 1]
 
-        return 0
+            for i in range(len(rr)):
+                rr[i] += min(r[i], r[i + 1])
+
+        return triangle[0][0]
 
 if __name__ == '__main__':
     tri = [[2], [3,4], [6,5,7], [4,1,8,3] ]
