b0288da2c615e57c7b825c34cbab9fb70543b13c
Yubin Bai <ybai@thrillist.com>
2014-04-25 15:11:07 -0400

    scramble

--- scrambleString/main.py
+++ scrambleString/main.py
@@ -46,37 +46,37 @@ Created on 2013-5-16
 
 @author: Yubin Bai
 '''
-
-
-def isScramble(s1, s2):
-    def _isScramble(s1Left, s2Left, length):
-        '''
-        starting s1Left and s2Left, decide whether the 
-        substrings with length are scrambled
-        '''
-        t = (s1Left, s2Left, length)
-        if t in memo:
-            return memo[t]
-        if s1[s1Left:s1Left + length] == s2[s2Left:s2Left + length]:
-            memo[t] = True
-            return True
-
-        for i in range(1, length):
-            if _isScramble(s1Left, s2Left, i) and \
-                    _isScramble(s1Left + i, s2Left + i, length - i):
+class Solution:
+    # @return a boolean
+    def isScramble(self, s1, s2):
+        # starting left1 and left2, decide whether the substrings with length are scrambled
+        def _isScramble(left1, left2, length):
+            t = (left1, left2, length)
+            if t in memo:
+                return memo[t]
+            if s1[left1:left1 + length] == s2[left2:left2 + length]:
                 memo[t] = True
                 return True
-            if _isScramble(s1Left, s2Left + length - i - 1, i) and \
-                    _isScramble(s1Left + length - i - 1, s2Left, length - i):
-                memo[t] = True
-                return True
-        memo[t] = False
-        return False
 
-    memo = {}
-    return _isScramble(0, 0, len(s1))
+            for i in range(1, length):
+                if _isScramble(left1, left2, i) and _isScramble(left1 + i, left2 + i, length - i):
+                    memo[t] = True
+                    return True
+                if _isScramble(left1, left2 + length - i, i) and _isScramble(left1 + i, left2, length - i):
+                    memo[t] = True
+                    return True
+            memo[t] = False
+            return False
 
+        memo = {}
+        ret = _isScramble(0, 0, len(s1))
+        # for k in memo: print k, memo[k]
+        return ret
 
 if __name__ == '__main__':
-    print(isScramble('great', 'rgeat'))
-    print(isScramble('great', 'rgtae'))
+    sol = Solution()
+    # print(sol.isScramble('great', 'rgeat'))
+    # print(sol.isScramble('ba', 'ab'))
+
+    print(sol.isScramble('eat', 'tae'))
+    # print(sol.isScramble('great', 'rgtae'))
--- wordSearch/main.py
+++ wordSearch/main.py
@@ -3,46 +3,44 @@ Created on May 16, 2013
 @author: Yubin Bai
 '''
 
+class Solution:
+    # @param board, a list of lists of 1 length string
+    # @param word, a string
+    # @return a boolean
+    def exist(self, board, word):
+        # backtrack
+        def _search(x, y, step, word):
+            if step == len(word):
+                result[0] = True
+                return
+            if result[0] or x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
+                return
 
-def wordSearch(matrix, w):
-    '''
-    search for words in matrix
-    '''
-    def _search(x, y, step, w):
-        '''
-        backtrack
-        '''
-        if step == len(w):
-            result[0] = True
-            return
-        if result[0] or x < 0 or x >= len(matrix) or y < 0 or y >= len(matrix[0]):
-            return
+            if board[x][y] == word[step]:
+                temp = board[x][y]
+                board[x][y] = -1
+                _search(x - 1, y, step + 1, word)
+                _search(x + 1, y, step + 1, word)
+                _search(x, y + 1, step + 1, word)
+                _search(x, y - 1, step + 1, word)
+                board[x][y] = temp
 
-        if matrix[x][y] == w[step]:
-            temp = matrix[x][y]
-            matrix[x][y] = -1
-            _search(x - 1, y, step + 1, w)
-            _search(x + 1, y, step + 1, w)
-            _search(x, y + 1, step + 1, w)
-            _search(x, y - 1, step + 1, w)
-            matrix[x][y] = temp
-
-    result = [False]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            _search(i, j, 0, w)
-            if result[0]:
-                return True
-    return False
+        result = [False]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                _search(i, j, 0, word)
+                if result[0]:
+                    return True
+        return False
 
 
 if __name__ == '__main__':
-    matrix = [
+    board = [
         list("ABCE"),
         list("SFCS"),
         list("ADEE")
     ]
     words = ['SEE', 'ABCB', 'ABCCED']
-
-    for w in words:
-        print(wordSearch(matrix, w))
+    sol = Solution()
+    for word in words:
+        print(sol.exist(board, word))
