5113f73e98066d842c8b0cf37af0d25fe4a7dd43
Yubin Bai <baiyubin@gmail.com>
2014-04-27 16:02:51 -0400

    str

--- longestCommonPrefix/main.py
+++ longestCommonPrefix/main.py
@@ -1,21 +1,23 @@
 # Longest Common Prefix
 # Write a function to find the longest common prefix string amongst an
 # array of strings.
+class Solution:
+    # @return a string
+    def longestCommonPrefix(self, strs):
+        def commonPrefix(s1, s2):
+            n = min(len(s1), len(s2))
+            pref = []
+            for i in range(n):
+                if s1[i] != s2[i]:
+                    break
+                else:
+                    pref.append(s1[i])
+            return ''.join(pref)
+        if len(strs) == 0:
+            return ''
+        return reduce(commonPrefix, strs)
 
-
-def commonPrefix(s1, s2):
-    n = min(len(s1), len(s2))
-    pref = []
-    for i in range(n):
-        if s1[i] != s2[i]:
-            break
-        else:
-            pref.append(s1[i])
-    return ''.join(pref)
-
-
-def longestCommon(s):
-    return reduce(commonPrefix, s)
-
-s = ['abc', 'abcde', 'abcd', 'acd', 'oo']
-print longestCommon(s)
+if __name__ == '__main__':
+    sol = Solution()
+    s = ['abc', 'abcde', 'abcd', 'acd', 'aoo']
+    print sol.longestCommonPrefix(s)
