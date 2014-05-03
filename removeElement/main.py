c82fc797b5f3d2ef429cf8e125f2f936bd683d24
Yubin Bai <baiyubin@gmail.com>
2014-05-03 09:39:30 -0400

    remove element

--- removeElement/main.py
+++ removeElement/main.py
@@ -10,22 +10,28 @@ Created on 2013-5-16
 '''
 
 
-def removeElement(data, x):
-    counter = left = right = 0
-    for right in range(len(data)):
-        if data[right] != x:
-            if left != right:
-                data[left] = data[right]
-            left += 1
-        else:
-            counter += 1
-    # remove rest of list
-    for _ in range(counter):
-        data.pop()
-    return left
+class Solution:
+    # @param    A       a list of integers
+    # @param    elem    an integer, value need to be removed
+    # @return an integer
+
+    def removeElement(self, A, elem):
+        counter = left = right = 0
+        for right in range(len(A)):
+            if A[right] != elem:
+                if left != right:
+                    A[left] = A[right]
+                left += 1
+            else:
+                counter += 1
+        # remove rest of list
+        for _ in range(counter):
+            A.pop()
+        return left
 
 if __name__ == '__main__':
-    data = [1, 2, 2, 3, 4, 5, 6, 1, 2, 2, 7]
-    print(data)
-    print("len = %d" % removeElement(data, 2))
-    print(data)
+    s = Solution()
+    A = [1, 2, 2, 3, 4, 5, 6, 1, 2, 2, 7]
+    print(A)
+    print("len = %d" % s.removeElement(A, 2))
+    print(A)
