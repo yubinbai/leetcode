f0f2e87ca6c02a86f9be9c637bee31dfb44f1738
Yubin Bai <ybai@thrillist.com>
2014-05-09 16:53:34 -0400

    balance tree

---
+++ balanced_binary_tree/main.py
@@ -0,0 +1,33 @@
+# Definition for a  binary tree node
+import collections
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a boolean
+    def isBalanced(self, root):
+        def _isBalanced(node):
+            if node == None:
+                return (True, 0, 0)
+            else:
+                r1, left1, right1 = _isBalanced(node.left)
+                r2, left2, right2 = _isBalanced(node.right)
+                left = max(left1, right1)
+                right = max(left2, right2)
+                if r1 and r2 and abs(left - right) <= 1:
+                    return (True, left + 1, right + 1)
+                else:
+                    return (False, 0, 0)
+
+        return _isBalanced(root)[0]      
+
+if __name__ == '__main__':
+    s = Solution()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.left.left = TreeNode(4)
+    print s.isBalanced(root)
