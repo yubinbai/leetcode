73e34c440e2719c52b9e07bf80f72739e4df7c58
Yubin Bai <baiyubin@gmail.com>
2014-06-28 09:56:59 -0400

    next permutation

--- nextPermutation/main.py
+++ nextPermutation/main.py
@@ -9,52 +9,51 @@
 
 # Here are some examples. Inputs are in the left-hand column and
 # its corresponding outputs are in the right-hand column.
-# 1,2,3 → 1,3,2
-# 3,2,1 → 1,2,3
-# 1,1,5 → 1,5,1
+# 1,2,3 => 1,3,2
+# 3,2,1 => 1,2,3
+# 1,1,5 => 1,5,1
 '''
 Created on May 17, 2013
 @author: Yubin Bai
 '''
 
 
-def nextPermutation(seq):
-    '''
-    permute the sequence in place, allows duplicate elements
-    raise error if reached end of permutation
-    '''
-    size = len(seq)
-    if size <= 1:
-        return
-    # 1.find the first acending order pair from the bottom
-    low = size - 2
-    high = size - 1
-    while seq[low] >= seq[high]:
-        if low == high - 1:
-            low -= 1
-            high = size - 1
-            if low < 0:
-                break
-        else:
-            high -= 1
-    if low < 0:
-        raise Exception("End of permutation")
+class Solution:
+    # permute the sequence in place, allows duplicate elements
+    # raise error if reached end of permutation
+    # @param num, a list of integer
+    # @return a list of integer
 
-    # 2.swap num[lo] with num[hi] if lo >= 0
-    # print low, high
-    seq[low], seq[high] = seq[high], seq[low]
+    def nextPermutation(self, num):
+        size = len(num)
+        if size <= 1:
+            return num
+        # 1.find the first acending order pair from the bottom
+        low = size - 2
+        high = size - 1
+        while num[low] >= num[high]:
+            if low == high - 1:
+                low -= 1
+                high = size - 1
+                if low < 0:
+                    break
+            else:
+                high -= 1
+        if low < 0:
+            num.sort()
+            return num
 
-    # 3.reverse lo+1 .. end
-    seq[low + 1:size] = seq[size - 1:low:-1]
+        # 2.swap num[lo] with num[hi] if lo >= 0
+        # print low, high
+        num[low], num[high] = num[high], num[low]
+
+        # 3.reverse lo+1 .. end
+        num[low + 1:size] = num[size - 1:low:-1]
+        return num
 
 if __name__ == '__main__':
     seq = [1, 2, 2, 3]
     print(seq)
+    s = Solution()
     for i in range(29):
-        try:
-            nextPermutation(seq)
-        except:
-            print("End of permutations")
-            break
-        else:
-            print(seq)
+        print s.nextPermutation(seq)
