e9183ca22b250cbd96199df8df2e41eae58501d2
Yubin Bai <baiyubin@gmail.com>
2014-07-04 10:54:12 -0400

    valid paren

--- longestValidParentheses/main.py
+++ longestValidParentheses/main.py
@@ -14,26 +14,31 @@ Created on May 20, 2013
 '''
 
 
-def longestValid(array):
-    if array == None or len(array) == 0:
-        return 0, 0, 0
-    maxLen = 0, 0, 0
-    stack = []
-    for right in range(len(array)):
-        if stack and array[stack[-1]] == '(' and array[right] == ')':
-            stack.pop()
-            left = -1
-            if stack:
-                left = stack[-1]
-            if maxLen[0] < right - left:
-                maxLen = right - left, left + 1, right
-        else:
-            stack.append(right)
-    return maxLen
+class Solution:
+    # @param s, a string
+    # @return an integer
+
+    def longestValidParentheses(self, s):
+        if s == None or len(s) == 0:
+            return 0
+        maxLen = 0, 0, 0
+        stack = []
+        for right in range(len(s)):
+            if stack and s[stack[-1]] == '(' and s[right] == ')':
+                stack.pop()
+                left = -1
+                if stack:
+                    left = stack[-1]
+                if maxLen[0] < right - left:
+                    maxLen = right - left, left + 1, right
+            else:
+                stack.append(right)
+        return maxLen[0]
 
 
 if __name__ == '__main__':
-    print(longestValid(')()())'))
-    print(longestValid('((()'))
-    print(longestValid('((()()()'))
-    print(longestValid('((()()()))'))
+    sol = Solution()
+    print(sol.longestValidParentheses(')()())'))
+    print(sol.longestValidParentheses('((()'))
+    print(sol.longestValidParentheses('((()()()'))
+    print(sol.longestValidParentheses('((()()()))'))
