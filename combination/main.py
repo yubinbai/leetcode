64fbfca6b9a5358a649dd1cd4e04b2cc5cb615a3
Yubin Bai <baiyubin@gmail.com>
2014-08-25 23:59:36 -0400

    Four

--- combination/main.py
+++ combination/main.py
@@ -1,20 +1,6 @@
-# Given two integers n and k, return all possible combinations of k
-# numbers out of 1 ... n.
-
-# For example,
-# If n = 4 and k = 2, a solution is:
-
-# [
-#   [2,4],
-#   [3,4],
-#   [2,3],
-#   [1,2],
-#   [1,3],
-#   [1,4],
-# ]
-
 class Solution:
     # @return a list of lists of integers
+
     def combine(self, n, k):
         results = []
         stack = []
@@ -31,6 +17,7 @@ class Solution:
         comb(1, 0)
         return results
 
-s = Solution()
-print s.combine(1, 0)
-print s.combine(4, 2)
+if __name__ == '__main__':
+    s = Solution()
+    print s.combine(1, 0)
+    print s.combine(4, 2)
--- combinationSum/main.py
+++ combinationSum/main.py
@@ -1,27 +1,15 @@
-# Given a set of candidate numbers (C) and a target number (T), find all
-# unique combinations in C where the candidate numbers sums to T.
-
-# The same repeated number may be chosen from C unlimited number of times.
-
-# Note:
-# All numbers (including target) will be positive integers.
-# Elements in a combination (a1, a2,ak) must be in non-descending order.
-# The solution set must not contain duplicate combinations.
-# For example, given candidate set 2,3,6,7 and target 7,
-# A solution set is:
-# [7]
-# [2, 2, 3]
-
 '''
 Created on May 16, 2013
 
 @author: Yubin Bai
 '''
 
+
 class Solution:
     # @param candidates, a list of integers
     # @param target, integer
     # @return a list of lists of integers
+
     def combinationSum(self, candidates, target):
 
         def _combinationSum(target, step):
@@ -49,6 +37,6 @@ if __name__ == '__main__':
     s = Solution()
     data = [2, 3, 6, 7]
     data = [1, 2]
-    target = 3 
+    target = 3
     results = s.combinationSum(data, target)
     print results
