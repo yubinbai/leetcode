2bb4cbc35ac35235ca42ec667bc49e0ea03bd0b0
Yubin Bai <baiyubin@gmail.com>
2014-08-30 18:34:07 -0400

    Area

--- jumpGameII/README.md
+++ jumpGameII/README.md
@@ -11,3 +11,12 @@ Your goal is to reach the last index in the minimum number of jumps.
 	Given array A = [2,3,1,1,4]
 
 	The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
+
+Solution
+===
+
+Iterate the list, maintain 3 variables:
+
+    ret # number of steps
+    last # max distance reached at ret steps
+    curr # max distance reached at ret+1 steps
--- largestRectangleHistogram/Solution.java
+++ largestRectangleHistogram/Solution.java
@@ -16,28 +16,14 @@ public class Solution {
         }
         return maxArea;
     }
-    public int largestRectangleAreaWrongAnswer(int[] height) {
-        int size = height.length;
-        if (size == 0) return 0;
-        if (size == 1) return height[0];
-        int ret = 0;
-        for (int left = 0; left < size; left++) {
-            if (left + 1 < size && height[left] >= height[left + 1]) continue;
-            int min = height[left];
-            for (int len = 1; (len + left - 1) < size; len++) {
-                min = Math.min(min, height[len + left - 1]);
-                ret = Math.max(ret, min * len);
-            }
-        }
-        return ret;
-    }
+
     public static void main(String[] args) {
         int[] ar = {1, 1};
-        // int[] ar = {2,1,5,6,2,3};
-        // int[] ar = new int[20000];
-        // for (int i = 0; i < 20000; i++) {
-        //     ar[i] = i;
-        // }
+        int[] ar = {2, 1, 5, 6, 2, 3};
+        int[] ar = new int[20000];
+        for (int i = 0; i < 20000; i++) {
+            ar[i] = i;
+        }
         Solution s = new Solution();
         System.out.println(s.largestRectangleArea(ar));
     }
