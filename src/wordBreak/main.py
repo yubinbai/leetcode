78d52449fee58cf44aee9b43ead592a7c64f2156
Yubin Bai <ybai@thrillist.com>
2015-01-26 17:48:57 -0500

    Remove numpy

--- src/wordBreak/main.py
+++ src/wordBreak/main.py
@@ -1,35 +1,29 @@
-# Given a string s and a dictionary of words dict, determine if s can be
-# segmented into a space-separated sequence of one or more dictionary
-# words.
-
-# For example, given
-# s = "leetcode",
-# dict = ["leet", "code"].
-
-# Return true because "leetcode" can be segmented as "leet code".
-import numpy as np
-
-
-def wordBreak(s, d):
-    def dp(a, b):
-        if memo[a, b] != -1:
-            return memo[a, b]
-        if s[a:b + 1] in d:
-            memo[a, b] = 1
-            return 1
-        for i in range(a, b):
-            if dp(a, i) > 0 and dp(i + 1, b) > 0:
-                memo[a, b] = 1
+class Solution:
+    # @param s, a string
+    # @param dict, a set of string
+    # @return a boolean
+    def wordBreak(self, s, dict):
+        
+        def dp(a, b):
+            if (a, b) in memo:
+                return memo[(a, b)]
+            if s[a:b + 1] in hash_set:
+                memo[(a, b)] = 1
                 return 1
-        memo[a, b] = 0
-        return 0
+            for i in range(a, b):
+                if dp(a, i) > 0 and dp(i + 1, b) > 0:
+                    memo[(a, b)] = 1
+                    return 1
+            memo[(a, b)] = 0
+            return 0
 
-    n = len(s)
-    memo = np.zeros((n, n), dtype=np.int)
-    memo.fill(-1)
-    return dp(0, n - 1) > 0
+        n = len(s)
+        memo = {}
+        hash_set = set(dict)
+        return dp(0, n - 1) > 0
 
 if __name__ == '__main__':
     s = "leetcodeleetcodecode"
     d = ["leet", "code", "abacus"]
-    print wordBreak(s, d)
+    sol = Solution()
+    print sol.wordBreak(s, d)
