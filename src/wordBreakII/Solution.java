c34b649196b159d8ccfcece2e7cdb7d628ec4017
Yubin Bai <ybai@thrillist.com>
2015-01-26 20:13:41 -0500

    Improve

--- src/wordBreakII/Solution.java
+++ src/wordBreakII/Solution.java
@@ -5,18 +5,27 @@ public class Solution {
     Set<String> dict;
     int n;
     boolean[] possible;
+    ArrayList<Integer> stack;
 
-    private void getAllSolution(int start, String result) {
+    private void getAllSolution(int start) {
         if (start == n) {
-            solutions.add(result.trim()); //eliminate the last space
+            StringBuilder sb = new StringBuilder(target);
+            for (int i = stack.size() - 1; i >= 0; --i) {
+                sb.insert(stack.get(i), " ");
+            }
+            solutions.add(sb.toString().trim());
             return;
         }
         for (int i = start; i < n; ++i) {
             String piece = target.substring(start, i + 1);
             if (dict.contains(piece) && possible[i + 1]) { 
                 int beforeChange = solutions.size();
-                getAllSolution(i + 1, result + " " + piece);
-                if (solutions.size() == beforeChange) possible[i + 1] = false;
+                stack.add(i + 1);
+                getAllSolution(i + 1);
+                stack.remove(stack.size() - 1);
+                if (solutions.size() == beforeChange) {
+                    possible[i + 1] = false;
+                }
             }
         }
     }
@@ -28,20 +37,21 @@ public class Solution {
         this.solutions = new ArrayList<String>();
         this.possible = new boolean[n + 1];
         Arrays.fill(possible, true);
-        this.getAllSolution(0, "");
+        this.stack = new ArrayList<Integer>();
+        getAllSolution(0);
         return solutions;
     }
 
     public static void main(String[] args) {
-        String[] dict = new String[] {"cat", "cats", "and", "sand", "dog"};
-        String target = "catsanddog";
-        // String[] dict = new String[] {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
-        // String target = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+        // String[] dict = new String[] {"cat", "cats", "and", "sand", "dog"};
+        // String target = "catsanddog";
+        String[] dict = new String[] {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
+        String target = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
         Set<String> set = new HashSet<String>();
         for (String s : dict)
             set.add(s);
         Solution sol = new Solution();
         // for (String s : set) System.out.println(s);
-        for (String s : sol.wordBreak(target, set)) System.out.println(s);
+        for (String s : sol.wordBreak(target, set)) System.out.format("__%s__\n", s);
     }
 }
