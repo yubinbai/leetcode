cd65a6e749056bc283c90593461ae6b88146b288
Yubin Bai <ybai@thrillist.com>
2015-01-26 20:26:44 -0500

    Naming

--- src/wordBreakII/Solution.java
+++ src/wordBreakII/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     Set<String> dict;
     int n;
     boolean[] possible;
-    ArrayList<Integer> stack;
+    Stack<Integer> stack;
 
     private void getAllSolution(int start) {
         if (start == n) {
@@ -20,9 +20,9 @@ public class Solution {
             String piece = target.substring(start, i + 1);
             if (dict.contains(piece) && possible[i + 1]) { 
                 int beforeChange = solutions.size();
-                stack.add(i + 1);
+                stack.push(i + 1);
                 getAllSolution(i + 1);
-                stack.remove(stack.size() - 1);
+                stack.pop();
                 if (solutions.size() == beforeChange) {
                     possible[i + 1] = false;
                 }
@@ -37,16 +37,16 @@ public class Solution {
         this.solutions = new ArrayList<String>();
         this.possible = new boolean[n + 1];
         Arrays.fill(possible, true);
-        this.stack = new ArrayList<Integer>();
+        this.stack = new Stack<Integer>();
         getAllSolution(0);
         return solutions;
     }
 
     public static void main(String[] args) {
-        // String[] dict = new String[] {"cat", "cats", "and", "sand", "dog"};
-        // String target = "catsanddog";
-        String[] dict = new String[] {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
-        String target = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+        String[] dict = new String[] {"cat", "cats", "and", "sand", "dog"};
+        String target = "catsanddog";
+        // String[] dict = new String[] {"a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"};
+        // String target = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
         Set<String> set = new HashSet<String>();
         for (String s : dict)
             set.add(s);
