3e63bf7e316e798650d74af93bcef7cf402f3796
Yubin Bai <baiyubin@gmail.com>
2015-02-08 11:00:44 -0500

    Better solution

--- src/mergeKSorted/README.md
+++ src/mergeKSorted/README.md
@@ -4,4 +4,13 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
 
 Solution
 ====
-Put all lists into a queue, while queue size is bigger than one, pop two lists from queue, merge them to one, and put back to queue
+Solution 1
+---
+Put all lists into a queue, while queue size is bigger than one, pop two lists from queue, merge them to one, and put back to queue. O(n lg k)
+
+
+Solution 2
+---
+Maintain a heap of k elements, with invariant of every lists' top node 
+
+For each iteration, pop the top node in the heap at each loop and add a new node from the same original list. O(n lg k)
--- src/mergeKSorted/Solution.java
+++ src/mergeKSorted/Solution.java
@@ -41,7 +41,7 @@ public class Solution {
         ListNode l1, l2;
         int n = lists.size();
         if (n == 0) {
-        	return null;
+            return null;
         }
         for (ListNode e : lists) {
             if (e != null) {
@@ -61,6 +61,46 @@ public class Solution {
             e = e.next;
         }
     }
+    public ListNode mergeKListsSolution2(List<ListNode> lists) {
+        if (lists.size() == 0)
+            return null;
+
+        //PriorityQueue is a sorted queue
+        PriorityQueue<ListNode> q = new PriorityQueue<ListNode>(lists.size(),
+        new Comparator<ListNode>() {
+            public int compare(ListNode a, ListNode b) {
+                if (a.val > b.val)
+                    return 1;
+                else if (a.val == b.val)
+                    return 0;
+                else
+                    return -1;
+            }
+        });
+
+        //add first node of each list to the queue
+        for (ListNode list : lists) {
+            if (list != null)
+                q.add(list);
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode p = head; // serve as a pointer/cursor
+
+        while (q.size() > 0) {
+            ListNode temp = q.poll();
+            //poll() retrieves and removes the head of the queue - q.
+            p.next = temp;
+
+            //keep adding next element of each list
+            if (temp.next != null)
+                q.add(temp.next);
+
+            p = p.next;
+        }
+
+        return head.next;
+    }
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(3);
@@ -70,7 +110,7 @@ public class Solution {
         ArrayList<ListNode> lists = new ArrayList<ListNode>();
         lists.add(l1);
         lists.add(l2);
-        ListNode res = s.mergeKLists(lists);
+        ListNode res = s.mergeKListsSolution2(lists);
         s.printList(res);
     }
 }
