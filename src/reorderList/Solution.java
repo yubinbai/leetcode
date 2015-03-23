b494b78a3f21bcc483b46b7b25d184e1c2c8dd75
Yubin Bai <ybai@thrillist.com>
2015-03-23 17:04:25 -0400

    Simplify

--- src/reorderList/Solution.java
+++ src/reorderList/Solution.java
@@ -1,34 +1,24 @@
 public class Solution {
     public void reorderList(ListNode head) {
-        ListNode curr = head, midHead;
-        int len = 0, mid;
+        if (head == null || head.next == null) return;
 
-        // find length
-        while (curr != null) {
-            len++;
-            curr = curr.next;
-        }
-        if (len <= 2) return;
-
-        if (len % 2 == 1) {
-            mid = (len - 1) / 2 + 2;
-        } else {
-            mid = len / 2 + 1;
+        ListNode slow = head, fast = head.next.next;
+        while (fast != null) {
+            if (fast.next == null) break;
+            fast = fast.next.next;
+            slow = slow.next;
         }
 
-        // break list in two parts, and reverse second half
-        midHead = reverseBetween(head, mid, len);
+        ListNode curr2 = reverse(slow.next);
+        slow.next = null;
 
-        // interleave
-        curr = head;
-        ListNode curr1 = head.next, curr2 = midHead;
+        ListNode curr = head, curr1 = head.next;
         while (curr1 != null || curr2 != null) {
             if (curr2 != null) {
                 curr.next = curr2;
                 curr2 = curr2.next;
                 curr = curr.next;
             }
-
             if (curr1 != null) {
                 curr.next = curr1;
                 curr1 = curr1.next;
@@ -38,44 +28,17 @@ public class Solution {
         curr.next = null;
     }
 
-    /**
-     * reverseBetween a linked list (indexed 1..n)
-     *
-     * @param  head [description]
-     * @param  m    [description]
-     * @param  n    [description]
-     * @return start of reversed part
-     */
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode curr, prev, oldList, newListHead, newListTail;
-        int counter;
+    public ListNode reverse(ListNode head) {
+        ListNode swap = null, oldList = head, newList = null;
 
-        // pass m - 1 nodes
-        curr = head;
-        prev = null;
-        for (int i = 0; i < m - 1; i++) {
-            prev = curr;
-            curr = curr.next;
+        while (oldList != null) {
+            swap = oldList;
+            oldList = oldList.next;
+            swap.next = newList;
+            newList = swap;
         }
 
-        // reverse list
-        counter = n - m + 1;
-        oldList = curr.next;
-        newListHead = curr;
-        newListTail = curr;
-
-        while (counter > 1) {
-            curr = oldList;
-            oldList = curr.next;
-            curr.next = newListHead;
-            newListHead = curr;
-            counter -= 1;
-        }
-
-        // break list in two
-        newListTail.next = oldList;
-        prev.next = null;
-        return newListHead;
+        return newList;
     }
 
     public void printList(ListNode e) {
