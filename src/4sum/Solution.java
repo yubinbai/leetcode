4ee1c60261aba5040e84b87bb0be37e6568760ff
Yubin Bai <ybai@thrillist.com>
2015-04-08 18:13:26 -0400

    simplify

--- src/4sum/Solution.java
+++ src/4sum/Solution.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Solution {
-    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+    public List<List<Integer>> fourSum(int[] num, int target) {
 
         Arrays.sort(num);
-        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
         int n = num.length;
         for (int i = 0; i < n - 3; i++) {
             if (i > 0 && num[i] == num[i - 1]) continue;
@@ -22,12 +22,7 @@ public class Solution {
                         while (k < n - 1 && num[k] == num[k + 1]) k++;
                         k++;
                     } else {
-                        ArrayList<Integer> listOfFour = new ArrayList<Integer>();
-                        listOfFour.add(num[i]);
-                        listOfFour.add(num[j]);
-                        listOfFour.add(num[k]);
-                        listOfFour.add(num[l]);
-                        ret.add(listOfFour);
+                        ret.add(Arrays.asList(num[i], num[j], num[k], num[l]));
                         while (l > 0 && num[l] == num[l - 1]) l--;
                         l--;
                         while (k < n - 1 && num[k] == num[k + 1]) k++;
@@ -43,11 +38,11 @@ public class Solution {
         Solution s = new Solution();
         int[] arr;
         arr = new int[] {1, 1, -1, 0, -1, 0, -2, 2};
-        for (ArrayList<Integer> a : s.fourSum(arr, 0)) {
+        for (List<Integer> a : s.fourSum(arr, 0)) {
             System.out.println(a);
         }
         arr = new int[] {0, 0, 0, 0};
-        for (ArrayList<Integer> a : s.fourSum(arr, 0)) {
+        for (List<Integer> a : s.fourSum(arr, 0)) {
             System.out.println(a);
         }
 
