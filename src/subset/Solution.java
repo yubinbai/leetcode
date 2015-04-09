e53a8ecae131def6bf18758b070a0a73a3d5d9c2
Yubin Bai <ybai@thrillist.com>
2015-04-09 18:07:21 -0400

    subset

--- src/subset/Solution.java
+++ src/subset/Solution.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Solution {
-    ArrayList<ArrayList<Integer>> ret;
+    List<List<Integer>> ret;
     Stack<Integer> curr;
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+    public List<List<Integer>> subsets(int[] S) {
         Arrays.sort(S);
-        ret = new ArrayList<ArrayList<Integer>>();
+        ret = new ArrayList<List<Integer>>();
         curr = new Stack<Integer>();
         _subsets(S, 0);
         return ret;
@@ -12,7 +12,7 @@ public class Solution {
     private void _subsets(int[] S, int step) {
         ret.add(new ArrayList<Integer>(curr));
         for (int i = step; i < S.length; ++i) {
-            if (i == step || (S[i - 1] != S[i])) {
+            if (i == step || S[i - 1] != S[i]) {
                 curr.push(S[i]);
                 _subsets(S, i + 1);
                 curr.pop();
@@ -22,7 +22,7 @@ public class Solution {
     public static void main(String[] args) {
         int[] S = new int[] {1, 2, 2, 3};
         Solution sol = new Solution();
-        for (ArrayList<Integer> a : sol.subsets(S)) {
+        for (List<Integer> a : sol.subsets(S)) {
             System.out.println(a);
         }
     }
