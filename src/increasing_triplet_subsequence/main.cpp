c2fed8bfb019b2148c2f8dc35fe3e26709608c8e
Yubin Bai <baiyubin@gmail.com>
2016-04-03 20:24:40 -0700

    334

---
+++ src/increasing_triplet_subsequence/main.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int>& nums) {
+    if (nums.empty()) {
+      return false;
+    }
+    int m1 = nums[0], m2 = INT_MAX;
+    int p2 = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+      if (nums[i] > m2) {
+        return true;
+      }
+      if (nums[i] > m1 && nums[i] < m2) {
+        m2 = nums[i];
+      } else if (nums[i] < m1) {
+        m1 = nums[i];
+      }
+    }
+    return false;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  vector<int> nums = {1, 2, 3, 4, 5};
+  cout << sol.increasingTriplet(nums) << "\n";
+
+  nums = {5, 4, 3, 2, 1};
+  cout << sol.increasingTriplet(nums) << "\n";
+
+  return 0;
+}
