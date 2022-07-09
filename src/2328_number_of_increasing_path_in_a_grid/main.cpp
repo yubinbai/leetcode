bf54582c73770ad1b630fd3c8f7ec5f79cee9e76
yubinbai <baiyubin@gmail.com>
2022-07-09 11:22:46 -0700

    simplify 2328

--- src/2328_number_of_increasing_path_in_a_grid/main.cpp
+++ src/2328_number_of_increasing_path_in_a_grid/main.cpp
@@ -10,68 +10,50 @@
 
 using namespace std;
 
-struct Cell {
-  int v;
-  int row;
-  int col;
-};
-
-struct cmpCell {
-  bool operator()(Cell a, Cell b) const {
-    return a.v < b.v;
-  }
-};
-
-const int MOD = 1000000007;
 
 class Solution {
 public:
+  int ret[1001][1001];
+  int m, n;
+  vector<vector<int>> g;
+  const int MOD = 1000000007;
+
+  int dfs(int row, int col) {
+    if (row < 0 || row >= m || col < 0 || col >= n) {
+      return 0;
+    }
+    if (ret[row][col] != 0) {
+      return ret[row][col];
+    }
+    ret[row][col] = 1;
+    if (row - 1 >= 0 && g[row - 1][col] > g[row][col]) {
+      ret[row][col] += dfs(row - 1, col);
+      ret[row][col] %= MOD;
+    }
+    if (row + 1 < m && g[row + 1][col] > g[row][col]) {
+      ret[row][col] += dfs(row + 1, col);
+      ret[row][col] %= MOD;
+    }
+    if (col - 1 >= 0 && g[row][col - 1] > g[row][col]) {
+      ret[row][col] += dfs(row, col - 1);
+      ret[row][col] %= MOD;
+    }
+    if (col + 1 < n && g[row][col + 1] > g[row][col]) {
+      ret[row][col] += dfs(row, col + 1);
+      ret[row][col] %= MOD;
+    }
+    return ret[row][col];
+  }
+
   int countPaths(vector<vector<int>>& grid) {
-    priority_queue<Cell, vector<Cell>, cmpCell> pq;
-    int m = grid.size();
-    int n = grid[0].size();
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        struct Cell c = {grid[i][j], i, j};
-        pq.push(c);
-      }
-    }
-    int ret[1001][1001] = {0};
-    while (!pq.empty()) {
-      Cell c = pq.top();
-      pq.pop();
-      if (ret[c.row][c.col] == 0) {
-        ret[c.row][c.col] = 1;
-      }
-      int ii = c.row - 1;
-      int jj = c.col;
-      if (ii >= 0 && grid[ii][jj] > c.v) {
-        ret[c.row][c.col] += ret[ii][jj];
-        ret[c.row][c.col] %= MOD;
-      }
-      ii = c.row + 1;
-      jj = c.col;
-      if (ii < m && grid[ii][jj] > c.v) {
-        ret[c.row][c.col] += ret[ii][jj];
-        ret[c.row][c.col] %= MOD;
-      }
-      ii = c.row;
-      jj = c.col - 1;
-      if (jj >= 0 && grid[ii][jj] > c.v) {
-        ret[c.row][c.col] += ret[ii][jj];
-        ret[c.row][c.col] %= MOD;
-      }
-      ii = c.row;
-      jj = c.col + 1;
-      if (jj < n && grid[ii][jj] > c.v) {
-        ret[c.row][c.col] += ret[ii][jj];
-        ret[c.row][c.col] %= MOD;
-      }
-    }
+    g = grid;
+    m = grid.size();
+    n = grid[0].size();
+    memset(ret, 0, sizeof ret);
     int r = 0;
     for (int i = 0; i < m; i++) {
       for (int j = 0; j < n; j++) {
-        r += ret[i][j];
+        r += dfs(i, j);
         r %= MOD;
       }
     }
