5eb2b9423da587831e246db5373612bac1b34773
yubinbai <baiyubin@gmail.com>
2022-10-23 16:20:04 -0700

    [debug] add xcode project for debug

---
+++ xcode-project/leetcode-debugger/leetcode-debugger/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode-debugger
+//
+//  Created by Yubin Bai on 10/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int a = 100;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
